9f1af06470703f9dac65eb10f78cff14765e625a
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2022-12-29 10:50:56 +0530

    started working on UserProfile/:userName page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard/project/:projectId', component: ProjectComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'new', component: CreateNewComponent},
   {path: 'SignIn', component: LoginComponent},
-  {path: 'PageNotFound', component: PageNotFoundComponent},
+  {path: 'UserProfile/:userName', component: PageNotFoundComponent},
   {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
